0201da652c686be3a57c85ad7679075ce3b08be4
sigped12 <112166811+sigped12@users.noreply.github.com>
2022-09-02 13:39:41 +0200

    updated guessing game
    changed the name of guess the number to guessing game and updated the html and js

--- guess-the-number/numberguess.js
+++
@@ -1,8 +0,0 @@
-var knapp = document.querySelector("#knappID")
-var input = document.querySelector("#inputID")
-
-function sjekkTall(){
-    console.log(input.Value)
-}
-
-knapp.addEventListener("click", sjekkTall);
---
+++ guessing-game/guessinggame.js
@@ -0,0 +1,8 @@
+var number = Math.floor(Math.random() * 101);
+var knapp = document.querySelector("#knappID");
+var input = document.querySelector("#inputID");
+function sjekkTall(){
+    console.log(input.value)
+}
+
+knapp.addEventListener("click", sjekkTall);
