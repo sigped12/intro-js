80e23551483686f20498043afa9abd5829028772
sigped12 <112166811+sigped12@users.noreply.github.com>
2022-09-09 11:36:30 +0200

    Got the guessing game working
    Got the game working, made changes to the old code and added styling.

--- guessing-game/guessinggame.js
+++ guessing-game/guessinggame.js
@@ -1,8 +1,34 @@
+var attempts = 5;
 var randomnumber = Math.floor(Math.random() * 101);
-var knapp = document.querySelector("#knappID");
+console.log("randomnumber: " + randomnumber);
+var button = document.querySelector("#buttonID");
 var input = document.querySelector("#inputID");
-function sjekkTall(){
-    console.log(input.value)
+function checkNumber(){
+    console.log("input: " + input.value);
+    console.log("attempts: " + attempts);
+
+    if (input.value == randomnumber) {
+        console.log("correct");
+        document.getElementById("output").innerHTML="You guessed correct with " + attempts + " attempts left.";
+    } else if (input.value < randomnumber) {
+        attempts--;
+        console.log("too low");
+        document.getElementById("output").innerHTML="Too low. " + attempts + " attempts left";
+    } else {
+        attempts--;
+        console.log("too high");
+        document.getElementById("output").innerHTML="Too high. " + attempts + " attempts left";
+    }
+
+    if (attempts <= 0) {
+        console.log("no attempts left");
+        document.getElementById("output").innerHTML="No attempts left. The number was " + randomnumber;
+    }
+
 }
 
-knapp.addEventListener("click", sjekkTall);
+//function timer(){}
+
+
+button.addEventListener("click", checkNumber);
+//button.addEventListener("click", function(){setTimeout(myFunc, 60000)});
